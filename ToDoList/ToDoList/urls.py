9764c20af5a1c4504669f61824b35dc59304ca5f
qwetry4444 <mishkin_mi@edu.surgu.ru>
2024-10-08 11:30:06 +0500

    123

--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # подключаем маршруты для работы с токенами
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # подключаем маршруты из наших приложений
     path('', include('tasks.urls')),
     path('', include('users.urls')),
+    # подключаем маршруты авторизации
+    re_path(r'^api/', include('djoser.urls')),
+    re_path(r'^api/', include('djoser.urls.jwt')),
 ]
-
--- ToDoList/tasks/views.py
+++ ToDoList/tasks/views.py
@@ -5,6 +5,10 @@ from .serializers import TasksSerializer
 from rest_framework.viewsets import ModelViewSet
 
 
+from rest_framework.permissions import IsAuthenticated
+from .models import Tasks
+from .serializers import TasksSerializer
+from rest_framework.viewsets import ModelViewSet
 class TasksViewSet(ModelViewSet):
     # Класс сериализатора, который следует использовать для проверки
     # и десериализации входных данных, а также для сериализации выходных данных.
@@ -12,3 +16,12 @@ class TasksViewSet(ModelViewSet):
     # QuerySet представляет набор запросов к базе данных,
     # а его методы создают подобные запросы.
     queryset = Tasks.objects.all()
+    # разрешаем доступ только зарегистрированным пользователям
+    permission_classes = (IsAuthenticated,)
+    # отображаем элементы принадлежащие только текущему юзеру,
+    # для админа разрешаем все
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Tasks.objects.all()
+        else:
+            return Tasks.objects.filter(user=self.request.user)
